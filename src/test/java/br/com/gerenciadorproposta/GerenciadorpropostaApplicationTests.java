5e9902578d088e8b6e156d95d59e8e286a535944
duartevinicius91 <duartevinicius91@gmail.com>
2019-11-06 21:44:44 -0200

    first commit

---
+++ src/test/java/br/com/gerenciadorproposta/GerenciadorpropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorpropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
