9b09836bdc70dcbbaa9d6ada05cebcfb4faed61c
vinicius <duartevinicius@gmail.com>
2019-11-13 20:48:50 -0300

    controller cliente

---
+++ src/main/java/br/com/gerenciadorproposta/model/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.gerenciadorproposta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cliente {
+
+    @Id
+    private Long id;
+
+    private String cnpj;
+
+    private String razaoSocial;
+
+    private String telefone;
+
+    private String email;
+}
