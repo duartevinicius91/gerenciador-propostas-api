11040d9cb0b8a54f1e3478a22e6516337890c702
Igor Cemim <igor@cemim.com.br>
2019-11-28 01:19:57 -0300

    Ajustada config CORS

--- src/main/java/br/com/gerenciadorproposta/config/WebConfig.java
+++ src/main/java/br/com/gerenciadorproposta/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods(
+                    "GET", "POST", "PUT",
+                    "DELETE", "OPTIONS", "HEAD",
+                    "TRACE", "CONNECT"
+                );
             }
         };
     }
